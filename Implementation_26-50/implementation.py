348a14fe3e9688306df081fcbd6f9de2abacf42f
Sam Branka <sdbranka@me.com>
2021-10-06 14:50:46 -0400

    Jumping on the Clouds: Revisited complete

--- Implementation_26-50/implementation.py
+++ Implementation_26-50/implementation.py
@@ -15,3 +15,23 @@ def permutationEquation(p):
     return y
 
 # 2
+def jumpingOnClouds(c, k):
+    n = len(c)
+    e = 100
+    i = k%n
+    while i != 0:
+        # print(f"i = {i}")
+        # print(f"i%n = {i%n}")
+        if c[i] == 1:
+            e -= 3
+        else:
+            e -= 1
+        i = (i + k) % n
+        # print(f"i = {i}")
+        # print(f"i%n = {i%n}")
+    # include the last jump made
+    if c[i] == 1:
+        e -= 3
+    else:
+        e -= 1
+    return e
